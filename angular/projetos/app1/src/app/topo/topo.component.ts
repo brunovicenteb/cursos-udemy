d51a7822bf69902909337090f57695890632eeb0
Bruno Belchior <brunovicenteb@gmail.com>
2023-01-25 14:39:31 -0300

    Aula 86 do curso de Angular.

--- angular/projetos/app1/src/app/topo/topo.component.ts
+++ angular/projetos/app1/src/app/topo/topo.component.ts
@@ -3,6 +3,10 @@ import { Component } from "@angular/core"
 @Component({
     selector: "app-topo",
     templateUrl: "./topo.component.html"
+    // template: `
+    // <p>
+    //     Esse é o meu topo
+    // </p>`
 })
 export class TopoComponent {
 
