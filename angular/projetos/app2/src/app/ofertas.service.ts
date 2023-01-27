88004e657c9af326a47990f73db373f8e2d2b19f
Bruno Belchior <brunovicenteb@gmail.com>
2023-01-27 18:20:12 -0300

    Aula 154 do curso de Angular.

--- angular/projetos/app2/src/app/ofertas.service.ts
+++ angular/projetos/app2/src/app/ofertas.service.ts
@@ -1,65 +1,18 @@
-import { ResolveEnd } from "@angular/router"
 import { Oferta } from "./shared/oferta.model"
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class OfertasService {
-    public ofertas: Oferta[] =
-        [
-            {
-                id: 1,
-                categoria: "restaurante",
-                titulo: "Super Burger",
-                descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-                anunciante: "Original Burger",
-                valor: 29.90,
-                destaque: true,
-                imagens: [
-                    { url: "/assets/ofertas/1/img1.jpg" },
-                    { url: "/assets/ofertas/1/img2.jpg" },
-                    { url: "/assets/ofertas/1/img3.jpg" },
-                    { url: "/assets/ofertas/1/img4.jpg" }
-                ]
-            },
-            {
-                id: 2,
-                categoria: "restaurante",
-                titulo: "Cozinha Mexicana",
-                descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-                anunciante: "Mexicana",
-                valor: 32.90,
-                destaque: true,
-                imagens: [
-                    { url: "/assets/ofertas/2/img1.jpg" },
-                    { url: "/assets/ofertas/2/img2.jpg" },
-                    { url: "/assets/ofertas/2/img3.jpg" },
-                    { url: "/assets/ofertas/2/img4.jpg" }
-                ]
 
-            },
-            {
-                id: 4,
-                categoria: "diversao",
-                titulo: "Estância das águas",
-                descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-                anunciante: "Estância das águas",
-                valor: 31.90,
-                destaque: true,
-                imagens: [
-                    { url: "/assets/ofertas/3/img1.jpg" },
-                    { url: "/assets/ofertas/3/img2.jpg" },
-                    { url: "/assets/ofertas/3/img3.jpg" },
-                    { url: "/assets/ofertas/3/img4.jpg" },
-                    { url: "/assets/ofertas/3/img5.jpg" },
-                    { url: "/assets/ofertas/3/img6.jpg" }
-                ]
-            }
-        ]
-
-    public getOfertas(): Array<Oferta> {
-        return this.ofertas
+    constructor(private hppt: HttpClient) {
     }
 
-    public getOfertas2(): Promise<Oferta[]> {
+    public getOfertas(): Promise<Oferta[]> {
         return new Promise<Oferta[]>((resolve, reject) => {
+
+            let ofertas = this.hppt.get<Oferta[]>("http://localhost:3000/ofertas")
+            resolve( ofertas);
             let deuCerto = true
             if (deuCerto) {
                 setTimeout(() => resolve(this.ofertas), 3000);
@@ -67,19 +20,4 @@ export class OfertasService {
                 reject({ codigoErro: 404, mensagemErro: "Servidor não encontrado." })
             }
         })
-            .then((ofertas: Oferta[]) => {
-                console.log("Primeiro then")
-                return ofertas
-            })
-            .then((ofertas: Oferta[]) => {
-                console.log("Segundo then")
-                return new Promise<Oferta[]>((resolve2, reject2) => {
-                    setTimeout(() => resolve2(ofertas), 3000);
-                })
-            })
-            .then((ofertas: Oferta[]) => {
-                console.log("Terceiro then executado após 03 segunods porque estava aguardando um promisse.")
-                return ofertas
-            })
     }
-}
