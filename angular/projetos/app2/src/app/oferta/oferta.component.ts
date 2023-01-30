2d52830801fbc4290e4e2b264ff59b049f438f8a
Bruno Belchior <brunovicenteb@gmail.com>
2023-01-30 18:30:00 -0300

    Aula 169 do curso de Angular [Restante].

--- angular/projetos/app2/src/app/oferta/oferta.component.ts
+++ angular/projetos/app2/src/app/oferta/oferta.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css']
 })
-export class OfertaComponent {
+export class OfertaComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    console.log(this.route.snapshot.params['id'])
+  }
 }
