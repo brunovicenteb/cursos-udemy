5991763e599d677d11856adbddbe3919cadce484
Bruno Belchior <brunovicenteb@gmail.com>
2023-01-30 20:48:25 -0300

    Aula 174 do curso de Angular.

--- angular/projetos/app2/src/app/ofertas.service.ts
+++ angular/projetos/app2/src/app/ofertas.service.ts
@@ -1,6 +1,7 @@
 import { Oferta } from "./shared/oferta.model"
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
+import { URL_API } from "./app.api";
 
 @Injectable()
 export class OfertasService {
@@ -9,19 +10,19 @@ export class OfertasService {
     }
 
     public getOfertas(): Promise<Oferta[]> {
-        return this.hppt.get("http://localhost:3000/ofertas?destaque=true")
+        return this.hppt.get(`${URL_API}?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta)
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.hppt.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.hppt.get(`${URL_API}?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any) => resposta)
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
-        return this.hppt.get(`http://localhost:3000/ofertas?id=${id}`)
+        return this.hppt.get(`${URL_API}?id=${id}`)
             .toPromise()
             .then((resposta: any) => resposta.shift())
     }
