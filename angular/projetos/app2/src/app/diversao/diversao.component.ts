b56356864975958062c1f2a1ce0ef2f1411bfd81
Bruno Belchior <brunovicenteb@gmail.com>
2023-01-30 15:51:35 -0300

    Aula 165 do curso de Angular.

--- angular/projetos/app2/src/app/diversao/diversao.component.ts
+++ angular/projetos/app2/src/app/diversao/diversao.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [OfertasService]
 })
-export class DiversaoComponent {
+export class DiversaoComponent implements OnInit {
 
+  constructor(private ofertaService: OfertasService) {
+  }
+
+  public ofertas!: Oferta[];
+
+  ngOnInit(): void {
+    this.ofertaService.getOfertasPorCategira('diversao')
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas
+      })
+  }
 }
