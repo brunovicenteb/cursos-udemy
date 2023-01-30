f049a6a3068d38560879b9a04bdecf355243d16e
Bruno Belchior <brunovicenteb@gmail.com>
2023-01-30 18:17:22 -0300

    Aula 167 do curso de Angular [Restante].

---
+++ angular/projetos/app2/src/app/oferta/oferta.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oferta',
+  templateUrl: './oferta.component.html',
+  styleUrls: ['./oferta.component.css']
+})
+export class OfertaComponent {
+}
