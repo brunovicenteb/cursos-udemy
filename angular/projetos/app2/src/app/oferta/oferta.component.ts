b856762f9992c82bae6c70eafb43db043dc0810c
Bruno Belchior <brunovicenteb@gmail.com>
2023-02-01 22:18:15 -0300

    Aula 190 do curso de Angular.

--- angular/projetos/app2/src/app/oferta/oferta.component.ts
+++ angular/projetos/app2/src/app/oferta/oferta.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
-import { interval, Observable, Observer, Subscription } from 'rxjs';
-
 
 @Component({
   selector: 'app-oferta',
@@ -11,18 +9,11 @@ import { interval, Observable, Observer, Subscription } from 'rxjs';
   styleUrls: ['./oferta.component.css'],
   providers: [OfertasService]
 })
-export class OfertaComponent implements OnInit, OnDestroy {
+export class OfertaComponent implements OnInit {
 
   public oferta!: Oferta
-  private tempoObservableSubscription!: Subscription
-  private meuObservableTesteSubscription!: Subscription
 
   constructor(private ofertaService: OfertasService, private route: ActivatedRoute) {
-  }
-
-  ngOnDestroy() {
-    this.tempoObservableSubscription?.unsubscribe()
-    this.meuObservableTesteSubscription?.unsubscribe()
   }
 
   ngOnInit() {
@@ -30,28 +21,5 @@ export class OfertaComponent implements OnInit, OnDestroy {
       .then((oferta: Oferta) => {
         this.oferta = oferta
       })
-
-    /*
-    this.route.params.subscribe({
-      next: (v) => console.log('next: ', v),
-      error: (e) => console.error('next: ',e),
-      complete: () => console.info('Complete')
-    })
-    */
-
-    let tempo = interval(2000)
-    this.tempoObservableSubscription = tempo.subscribe(interval => console.log('Next: ', interval));
-
-    let meuObservableTeste = new Observable((observer: Observer<number>) => {
-      observer.next(1)
-      observer.next(2)
-      observer.complete()
-    })
-
-    this.meuObservableTesteSubscription = meuObservableTeste.subscribe({
-      next: (resultado: number) => console.log(resultado + 10),
-      error: (erro) => console.log(erro),
-      complete: () => console.log('Stream de eventos foi finalizada')
-    })
   }
 }
