61f90a5741358f1275934746d8ed854719f7ac67
Bruno Belchior <brunovicenteb@gmail.com>
2023-02-01 23:46:23 -0300

    Aula 194 do curso de Angular.

--- angular/projetos/app2/src/app/ofertas.service.ts
+++ angular/projetos/app2/src/app/ofertas.service.ts
@@ -2,40 +2,48 @@ import { Oferta } from "./shared/oferta.model"
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { URL_API } from "./app.api";
+import { map, Observable } from "rxjs";
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class OfertasService {
 
-    constructor(private hppt: HttpClient) {
+    constructor(private http: HttpClient) {
     }
 
     public getOfertas(): Promise<Oferta[]> {
-        return this.hppt.get(`${URL_API}/ofertas?destaque=true`)
+        return this.http.get(`${URL_API}/ofertas?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta)
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.hppt.get(`${URL_API}/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any) => resposta)
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
-        return this.hppt.get(`${URL_API}/ofertas?id=${id}`)
+        return this.http.get(`${URL_API}/ofertas?id=${id}`)
             .toPromise()
             .then((resposta: any) => resposta.shift())
     }
 
     public getComoUsarOfertaPorId(id: number): Promise<string> {
-        return this.hppt.get(`${URL_API}/como-usar?id=${id}`)
+        return this.http.get(`${URL_API}/como-usar?id=${id}`)
             .toPromise()
             .then((resposta: any) => resposta[0].descricao)
     }
 
     public getOndeFicaPorId(id: number): Promise<string> {
-        return this.hppt.get(`${URL_API}/onde-fica?id=${id}`)
+        return this.http.get(`${URL_API}/onde-fica?id=${id}`)
             .toPromise()
             .then((resposta: any) => resposta[0].descricao)
-    }    
+    }
+
+    public pesquisaOfertas(termo: string): Observable<Oferta[]> {
+        return this.http.get(`${URL_API}/ofertas?descricao_oferta=${termo}`).pipe(
+            map((resposta) => <Oferta[]>resposta)
+        )
+    }
 }
--- angular/projetos/app2/src/app/topo/topo.component.ts
+++ angular/projetos/app2/src/app/topo/topo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
 
 @Component({
   selector: 'app-topo',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
   }
