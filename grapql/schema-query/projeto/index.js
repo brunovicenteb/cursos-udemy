942a1257a073f10561922639800a9fc7450bcdef
Bruno Belchior <brunovicenteb@gmail.com>
2023-02-06 16:08:12 -0300

    Aula 17 do curso de GraphQL.

--- grapql/schema-query/projeto/index.js
+++ grapql/schema-query/projeto/index.js
@@ -2,8 +2,22 @@ const { ApolloServer, gql } = require("apollo-server")
 
 const typeDefs = gql`
     #Pontos de entrada da sua API!
+
+    scalar Date
+
+    type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     type Query {
         ola: String!
+        horaAtual: Date!
+        usuarioLogado: Usuario
     }
 `
 
@@ -11,6 +25,19 @@ const resolvers = {
     Query: {
         ola() {
             return 'Bom dia!'
+        },
+        horaAtual() {
+            return new Date()
+        },
+        usuarioLogado(){
+            return {
+                id: 1,
+                nome: 'Ana da Web',
+                email: 'anadawe@email.com',
+                idade: 23,
+                salario: 1234.56,
+                vip: true
+            }
         }
     }
 }
