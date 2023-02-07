aafb3a2654c5d924af0b2fa626dcf75692113d54
Bruno Belchior <brunovicenteb@gmail.com>
2023-02-07 10:02:32 -0300

    Aula 31 do curso de GraphQL.

--- grapql/schema-query/projeto/data/db.js
+++ grapql/schema-query/projeto/data/db.js
@@ -3,19 +3,22 @@ const usuarios = [{
     nome: 'João Silva',
     email: 'jsilva@zemail.com',
     idade: 29,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'ATIVO'
 }, {
     id: 2,
     nome: 'Rafael Junior',
     email: 'rafajun@wemail.com',
     idade: 31,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'INATIVO'
 }, {
     id: 3,
     nome: 'Daniel Smith',
     email: 'danismi@uemail.com',
     idade: 24,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'BLOQUEADO'
 }]
 
 const perfis = [{
